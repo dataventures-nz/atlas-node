3c7bc942d7e553fc07305522d00f739a350baba9
blair <blair.nilsson@gmail.com>
2020-02-21 14:01:00 +1300

    with health check

--- exec.sh
+++ exec.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 docker build -t dataventures/atlas-node .
-docker run --env-file ../.env -p 3010:3010 -it dataventures/atlas-node
+docker run --env-file ../.env -p 3000:3000 -it dataventures/atlas-node
--- server.js
+++ server.js
@@ -156,6 +156,7 @@ async function doQuery(req,res) {
 app.post('/api/:table', checkJwt, doQuery)
 app.get('/api/:table', checkJwt, doQuery)
 
+app.get('/health', (req,res) => res.send("ok"))
 
 app.use(function(err, req, res, next) {
   console.error(err.stack)
