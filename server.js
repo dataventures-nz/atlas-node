5ee4180d55f4bb1c96c469e7907782e9a26149a4
Blair Nilsson <blair.nilsson@gmail.com>
2020-08-18 12:29:18 +1200

    new version

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.post('/api/:table', checkJwt, checkTime, doQuery)
 app.get('/api/:table', checkJwt, checkTime, doQuery)
 app.get('/count/:table', checkJwt, checkTime, doCount)
 
-app.get('/health', (req,res) => res.send("ok - version 1.30c (bounce test)\n"))
+app.get('/health', (req,res) => res.send("ok - version 1.31 (count)\n"))
 
 // note.... THIS has to be fast. since we use it as part of the regular query system.
 // so it will only do one call, and not add a bunch of extra stuff to it.
