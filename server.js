3ae8aaaaa4ad6a5111ceb61f92ab16c37904da13
Blair Nilsson <blair.nilsson@gmail.com>
2020-09-01 15:46:48 +1200

    version upgrade

--- server.js
+++ server.js
@@ -225,7 +225,7 @@ app.post('/api/:table', checkJwt, checkTime, doQuery)
 app.get('/api/:table', checkJwt, checkTime, doQuery)
 app.get('/count/:table', checkJwt, checkTime, doCount)
 
-app.get('/health', (req,res) => res.send("ok - version 1.32 (limits removed)\n"))
+app.get('/health', (req,res) => res.send("ok - version 1.33 (compression)\n"))
 
 // note.... THIS has to be fast. since we use it as part of the regular query system.
 // so it will only do one call, and not add a bunch of extra stuff to it.
